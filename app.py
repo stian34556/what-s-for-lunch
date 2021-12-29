9f1068f5785d268da46600a459a9d22361e6b842
ericlin7 <80200462+ericlin7@users.noreply.github.com>
2021-12-29 12:41:24 -0800

    added navbar

--- app.py
+++ app.py
@@ -2,7 +2,31 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+FOOD = [
+    "American",
+    "Asian",
+    "Barbecue",
+    "Canadian",
+    "Chinese",
+    "Europen",
+    "Fast Food",
+    "French",
+    "Halal",
+    "Italian",
+    "Japanese",
+    "Korean",
+    "Mexican",
+    "Vegetarian",
+    "Vietnamese"
+]
+
+
+@app.route("/login", methods = ["POST", "GET"])
+def login():
+    return render_template("login.html")
+
+
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("home.html", suggested=FOOD)
 
