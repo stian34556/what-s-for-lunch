7bbc2ea65b538a5eb22d2a7986eeaca1e88292a0
ericlin7 <80200462+ericlin7@users.noreply.github.com>
2021-12-29 13:43:27 -0800

    yelp api

--- app.py
+++ app.py
@@ -1,32 +1,27 @@
+
 from flask import Flask, render_template, request
+from yelp_api import *
 
 app = Flask(__name__)
 
 FOOD = [
-    "American",
-    "Asian",
-    "Barbecue",
-    "Canadian",
-    "Chinese",
-    "Europen",
-    "Fast Food",
-    "French",
-    "Halal",
-    "Italian",
-    "Japanese",
-    "Korean",
-    "Mexican",
-    "Vegetarian",
-    "Vietnamese"
+    "American","Asian", "Barbecue","Chinese", "European",
+    "Fast Food","French","Halal", "Italian", "Japanese",
+    "Korean","Mexican","Vegetarian","Vietnamese"
 ]
-
 
 @app.route("/login", methods = ["POST", "GET"])
 def login():
     return render_template("login.html")
 
-
 @app.route("/")
 def home():
     return render_template("home.html", suggested=FOOD)
 
+@app.route("/find", methods = ["POST"])
+def find():
+    
+    print(request.form.get("Food"))
+    search_result()   
+
+    return render_template("find.html")
