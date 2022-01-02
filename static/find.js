ff31b6a63e8ad2f5f1651647b2fafe738f8db429
stian34556 <jytian34556@gmail.com>
2022-01-01 23:45:23 -0800

    my computer

--- static/find.js
+++ static/find.js
@@ -1,7 +1,33 @@
-function add_to_fav(){
-    
-    console.log(document.getElementById("one").innerHTML)
-    console.log(document.getElementById("two").innerHTML)
-    console.log(document.getElementById("three").innerHTML)
-   
+function add_to_fav(num){
+    var user = firebase.auth().currentUser;
+    if (user) {
+        if (num == 1) {
+            title = document.getElementById("one").innerHTML;
+            const FoodRef = firebase.firestore().collection("users").doc(user.uid)
+            .add({
+                title: "",
+            })
+            .then((ref) => {
+                console.log("Added doc with ID: ", ref.id);
+          });
+        } else if (num == 2) {
+            title = document.getElementById("two").innerHTML;
+            const FoodRef = firebase.firestore().collection("users").doc(user.uid)
+            .add({
+                title: "",
+            })
+            .then((ref) => {
+                console.log("Added doc with ID: ", ref.id);
+          });
+        } else {
+            title = document.getElementById("three").innerHTML;
+            const FoodRef = firebase.firestore().collection("users").doc(user.uid)
+            .add({
+                title: "",
+            })
+            .then((ref) => {
+                console.log("Added doc with ID: ", ref.id);
+          });
+        }      
+    }
 }
