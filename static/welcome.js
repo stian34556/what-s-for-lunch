d3b0aed127a5d86df5eb1d2d1b33d09f0a3ecbf4
ericlin7 <80200462+ericlin7@users.noreply.github.com>
2022-01-01 18:36:27 -0800

    console

--- static/welcome.js
+++ static/welcome.js
@@ -1,7 +1,21 @@
-firebase.auth().onAuthStateChanged((user)=>{
-    if(!user){
+const db = firebase.firestore();
+
+firebase.auth().onAuthStateChanged((user)=> {  
+    if (!user) {
         location.replace("login.html")
-    }else{
+    } else {
+        const bookRef = firebase
+        .firestore()
+        .collection("users")
+        .doc("test");
+
+        bookRef.get().then((doc) => {
+        if (!doc.exists) return;
+        console.log("Document data:", doc.data());
+        // Document data: { title: 'The Great Gatsby' }
+        });
+
+
         document.getElementById("user").innerHTML = "Hello, " + user.email
     }
 })
@@ -10,3 +24,6 @@ firebase.auth().onAuthStateChanged((user)=>{
 function logout(){
     firebase.auth().signOut()
 }
+
+
+
