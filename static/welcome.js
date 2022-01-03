0d40f5fcc52d47a671c13510d014fe92a8505559
stian34556 <jytian34556@gmail.com>
2022-01-03 14:18:40 -0800

    welcomeeee

--- static/welcome.js
+++ static/welcome.js
@@ -10,12 +10,14 @@ firebase.auth().onAuthStateChanged((user)=> {
         FoodRef.get().then((doc) => {
         if (!doc.exists) return;
 
-        json_to_html_tag.innerHTML =  "Fav Restaurant <br><br>";
-
+        json_to_html_tag.innerHTML =  "<h3>Past Restaurants </h3><br> ";
+        var li = document.createElement("li");
+        var ul = document.getElementById("json_to_html_tag");
         for (var key of Object.keys(doc.data())) {
-            console.log(key + " -> " + doc.data()[key])
             for (var test of doc.data()[key]) {
-                json_to_html_tag.innerHTML += "Restaurant Name: " + test + "<br><br>";
+                json_to_html_tag.innerHTML += "<li class=list-group-item>" + test + "</li>";
+
+                //json_to_html_tag.innerHTML += test + "<br><br>";
             }
 
         }
